dbead4de161da71168f3f8b4c82c84ceed2aee7d
Ilya <ilryaz08@gmail.com>
2026-04-21 18:23:45 +0300

    Added calender settings (temporarily tasks and progress bars don't show anything)

--- plugins/school/model.py
+++ plugins/school/model.py
@@ -1,18 +1,14 @@
-from datetime import date
-
 class Notebook:
-    def __init__(self, name, target_hours):
-        self.name = name
-        self.target_hours = target_hours
-
+    def __init__(self):
         self.data = {}
+        self.week_config = {}
 
-    def add_hours(self, hours, date):
+    def add_hours(self, hours, date, subject):
         week = self.get_current_week(date)
 
-        self.data[week] = self.data.setdefault(week, 0) + hours
+        self.data[week][subject]['current_hours'] = self.data.setdefault(week, 0) + hours
 
-        print(f'added {hours} hours to {week}\n{self.data[week]}')
+        print(f'added {hours} hours to {week}, subject {subject} current: {self.data[week][subject]['current_hours']}')
 
     def progress(self, date):
         week = self.get_current_week(date)
--- plugins/school/page.py
+++ plugins/school/page.py
@@ -1,16 +1,76 @@
 from PySide6.QtWidgets import (QWidget, QLabel, QPushButton,
                                QVBoxLayout, QHBoxLayout, QProgressBar,
-                               QCalendarWidget, QSplitter)
+                               QCalendarWidget, QSplitter, QDialog,
+                               QDialogButtonBox, QListWidget, QListWidgetItem,
+                               QInputDialog)
 from PySide6.QtCore import Qt
 from .model import Notebook
+
+class NewSubjectDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("New subject")
+        self.setFixedSize(250, 300)
+
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+
+        # lower widgets        
+        self.button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+
+        main_layout.addWidget(self.button_box)
+
+
+
+class WeekSettingsDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setMinimumWidth(400)
+        self.setMinimumHeight(250)
+
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+
+        # upper & central part
+        self.list_widget = QListWidget()
+
+        # lower part
+        lower_widget = QWidget()
+        lower_layout = QHBoxLayout()
+
+        self.add_button = QPushButton('Add')
+        self.add_button.clicked.connect(self.add_subject)
+
+        self.button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+
+        lower_layout.addWidget(self.add_button)
+        lower_layout.addWidget(self.button_box)
+
+        lower_widget.setLayout(lower_layout)
+
+        main_layout.addWidget(self.list_widget)
+        main_layout.addWidget(lower_widget)
+
+    def add_subject(self):
+        add_dialog = NewSubjectDialog()
+        add_dialog.exec()
+
+
+
+
 
 
 class SchoolPage(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.notebooks = {"Mathematics": Notebook("Mathematics", 11),
-                          "German": Notebook("German", 7)}
+        self.notebook = Notebook()
         self.progress_bars = {}
 
         main_layout = QVBoxLayout(self)
@@ -24,7 +84,12 @@ class SchoolPage(QWidget):
         # upper-left widgets
         upper_left_layout = QVBoxLayout()
 
+        self.calender_settings = QPushButton('Settings')
+        self.calender_settings.clicked.connect(self.open_calender_settings)
+
         self.calender = QCalendarWidget()
+
+        upper_left_layout.addWidget(self.calender_settings)
         upper_left_layout.addWidget(self.calender)
 
         upper_left_widget = QWidget()
@@ -33,8 +98,8 @@ class SchoolPage(QWidget):
         # upper-right widgets
         upper_right_layout = QVBoxLayout()
 
-        upper_right_layout.addLayout(self.create_subject_block('Mathematics'))
-        upper_right_layout.addLayout(self.create_subject_block('German'))
+        for subject in self.notebook.week_config:
+            upper_right_layout.addLayout(self.create_subject_block(subject))
 
         upper_right_widget = QWidget()
         upper_right_widget.setLayout(upper_right_layout)
@@ -42,7 +107,7 @@ class SchoolPage(QWidget):
         # lower widgets
         lower_layout = QVBoxLayout()
 
-        for subject in self.notebooks.keys():
+        for subject in self.notebook.week_config.keys():
             lower_layout.addWidget(self.create_weekly_progress_bar(subject))
 
         lower_widget = QWidget()
@@ -55,19 +120,19 @@ class SchoolPage(QWidget):
         main_splitter.addWidget(lower_widget)
         main_layout.addWidget(main_splitter)
 
-    def create_subject_block(self, name):
+    def create_subject_block(self, subject):
         layout = QHBoxLayout()
         
-        label = QLabel(name)
+        label = QLabel(subject)
         
         # buttons
         add_one_hour = QPushButton('+1 hour')
         add_thirty_minutes = QPushButton('+30 mins')
         add_fifteen_minutes = QPushButton('+15 mins')
 
-        add_one_hour.clicked.connect(lambda: self.handle_add_hours(name, 1))
-        add_thirty_minutes.clicked.connect(lambda: self.handle_add_hours(name, 0.5))
-        add_fifteen_minutes.clicked.connect(lambda: self.handle_add_hours(name, 0.25))
+        add_one_hour.clicked.connect(lambda: self.handle_add_hours(subject, 1))
+        add_thirty_minutes.clicked.connect(lambda: self.handle_add_hours(subject, 0.5))
+        add_fifteen_minutes.clicked.connect(lambda: self.handle_add_hours(subject, 0.25))
 
         layout.addWidget(label)
         layout.addWidget(add_one_hour)
@@ -76,27 +141,33 @@ class SchoolPage(QWidget):
 
         return layout
     
-    def handle_add_hours(self, name, hours):
+    def handle_add_hours(self, subject, hours):
         date = self.calender.selectedDate().toPython()
-        self.notebooks[name].add_hours(hours, date)
-        self.progress_bars[name].setValue(self.notebooks[name].progress(date))
+        self.notebooks[subject].add_hours(hours, date)
+        self.progress_bars[subject].setValue(self.notebooks[subject].progress(date))
 
 
-        print(name, hours, '\n') # откладка
+        print(subject, hours, '\n') # откладка
     
-    def create_weekly_progress_bar(self, name):
+    def create_weekly_progress_bar(self, subject):
         date = self.calender.selectedDate().toPython()
 
         progress_bar = QProgressBar()
 
         progress_bar.setMinimum(0)
-        progress_bar.setMaximum(self.notebooks[name].target_hours)
+        progress_bar.setMaximum(self.notebooks[subject].target_hours)
         
-        progress_bar.setFormat(f"{name}: %v / %m")
+        progress_bar.setFormat(f"{subject}: %v / %m")
 
-        current_value = self.notebooks[name].progress(date)
+        current_value = self.notebooks[subject].progress(date)
         progress_bar.setValue(current_value)
 
-        self.progress_bars[name] = progress_bar
+        self.progress_bars[subject] = progress_bar
         
         return progress_bar
+    
+    def open_calender_settings(self):
+        settings = WeekSettingsDialog(self)
+
+        if settings.exec() == QDialog.Accepted:
+            pass
