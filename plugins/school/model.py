009ebe467dde2c4a84924b2cb5f46fffef64a0a3
ilryaz <ilryaz08@gmail.com>
2026-06-08 22:27:16 +0300

    Created data.json, now all data stores to this file

--- plugins/school/model.py
+++ plugins/school/model.py
@@ -4,23 +4,29 @@ from pathlib import Path
 
 class Notebook:
     def __init__(self):
-        self.data = {}
+        DATA_FILE = Path(__file__).parent.parent.parent / "data" / "school" / "data.json"
+        with open(DATA_FILE, encoding="utf-8") as file:
+            self.data = json.load(file)
 
-        week_file = Path(__file__).parent.parent.parent / "data" / "school" / "week_config.json"
-        with open(week_file, encoding="utf-8") as file:
+        WEEK_FILE = Path(__file__).parent.parent.parent / "data" / "school" / "week_config.json"
+        with open(WEEK_FILE, encoding="utf-8") as file:
             self.week_config = json.load(file)
         
-        time_buttons_file = Path(__file__).parent.parent.parent / "data" / "school" / "time_buttons.json"
-        with open(time_buttons_file, encoding="utf-8") as file:
+        TIME_BUTTONS_FILE = Path(__file__).parent.parent.parent / "data" / "school" / "time_buttons.json"
+        with open(TIME_BUTTONS_FILE, encoding="utf-8") as file:
             self.time_buttons = json.load(file)
 
     def add_minutes(self, minutes, date, id):
         week = self.get_current_week(date)
+        DATA_FILE = Path(__file__).parent.parent.parent / "data" / "school" / "data.json"
 
         if week not in self.data:
             self.data[week] = {}
 
         self.data[week][id] = self.data[week].get(id, 0) + minutes
+
+        with open(DATA_FILE, "w", encoding="utf-8") as file:
+            json.dump(self.data, file, indent=4)
 
         print(f"added {minutes} minutes to {week}, id {id} current: {self.data[week][id]}")
 
